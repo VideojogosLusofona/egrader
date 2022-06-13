4b331bcfe07393d403eaae600a12712d22093b7d
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-06-13 17:08:24 +0100

    New cli: load and save validated student URLs

--- egrader/cli.py
+++ egrader/cli.py
@@ -3,6 +3,7 @@ from collections.abc import Sequence
 from pathlib import Path
 import shutil
 import sys
+from typing import Any
 
 import yaml
 
@@ -14,7 +15,7 @@ OPT_E_STOP = "stop"
 OPT_E_UPDT = "update"
 OPT_E_OVWR = "overwrite"
 
-FILE_REPO_REGISTRY = "repos.yml"
+FILE_VALIDATED_GIT_URLS = "validated_git_urls.yml"
 
 
 def main():
@@ -92,10 +93,7 @@ def main():
         args.func(args)
     except (FileNotFoundError, FileExistsError, SyntaxError) as e:
         print(e.args[0], file=sys.stderr)
-        # print(getattr(e, "message", repr(e)), file=sys.stderr)
         return 1
-    except Exception as e:
-        raise e
     else:
         print("Program terminated successfully.")
         return 0
@@ -105,70 +103,108 @@ def fetch(args) -> None:
     """Fetch operation: clone or update all repositories."""
 
     # Create file paths
-    urls_file = Path(args.urls_file[0])
-    rules_file = Path(args.rules_file[0])
+    urls_fp = Path(args.urls_file[0])
+    rules_fp = Path(args.rules_file[0])
 
     # Check if Git URLs file exists, and if not, quit
-    check_required_file_exists(urls_file)
+    check_required_fp_exists(urls_fp)
 
     # Check if rules file exists, and if not, quit
-    check_required_file_exists(rules_file)
+    check_required_fp_exists(rules_fp)
 
     # Determine output folder, either given by user or we extract it from the
     # rules file name
     if args.output_folder != None:
-        output_folder = Path(args.output)
+        output_fp = Path(args.output)
     else:
-        output_folder = Path(f"out_{rules_file.stem}")
+        output_fp = Path(f"out_{rules_fp.stem}")
+
+    # Create file path for validated URLs yaml file
+    validated_urls_fp = output_fp.joinpath(FILE_VALIDATED_GIT_URLS)
 
     # Check if output folder exists
-    if output_folder.exists():
+    if output_fp.exists():
+
         # If so, action to take depends on the -e command line option
         if getattr(args, OPT_E_LONG) == OPT_E_STOP:
+
             # Stop processing
             raise FileExistsError("Output folder already exists, stopping operation. "\
                 f"Check the -{OPT_E_SHORT}/--{OPT_E_LONG} option for alternative behavior.")
+
         elif  getattr(args, OPT_E_LONG) == OPT_E_OVWR:
+
             # Delete folder and its contents and recreate it
             print("Output folder already exists, deleting it...")
-            shutil.rmdir(output_folder)
-            output_folder.mkdir()
+            shutil.rmdir(output_fp)
+            output_fp.mkdir()
     else:
+
         # Folder doesn't exist, create it
-        output_folder.mkdir()
+        output_fp.mkdir()
 
     # Load rules
-    with open(rules_file, 'r') as rf:
-        repo_rules = yaml.safe_load(rf)
+    repo_rules = load_yaml(rules_fp)
 
     # Load student Git URLs
-    students = load_urls(urls_file)
+    if validated_urls_fp.exists():
 
+        # If file with validated URLs already exists, load info from there to
+        # avoid rechecking the URLs (only with "-e update" option)
+        students = load_yaml(validated_urls_fp)
 
-    #repo_registry_file = Path(args.output, FILE_REPO_REGISTRY)
+    else:
+
+        # Otherwise load info from original file and validate URLs
+        students = load_urls(urls_fp)
+
+        # Save validated URLs to avoid rechecking them later with the "-e update"
+        # option
+        save_yaml(validated_urls_fp, students)
+
+    print(students)
+
 
 def assess(args) -> None:
     print("ASSESS!")
     print(args)
 
 
-def check_required_file_exists(file_to_check: Path) -> None:
-    """ Check if file exists, and if not, quit"""
-    if not file_to_check.exists():
-        raise FileNotFoundError(f"File '{file_to_check}' does not exist!")
+def load_yaml(yaml_fp: Path) -> Any:
+    """Load a yaml file"""
+    try:
+        with open(yaml_fp, "r") as yaml_file:
+            yaml_obj = yaml.safe_load(yaml_file)
+    except yaml.scanner.ScannerError as se:
+        raise SyntaxError(f"Syntax error in file {yaml_fp} {se.args[0]}") from se
+
+    return yaml_obj
 
 
-def load_urls(urls_file: Path) -> Sequence[Student]:
+def save_yaml(yaml_fp: Path, data: Any) -> None:
+    """Save a yaml file"""
+
+    yaml_text = yaml.dump(data)
+    print(yaml_text, file=open(yaml_fp, "w"))
+
+
+def check_required_fp_exists(fp_to_check: Path) -> None:
+    """ Check if file path exists, and if not, raise exception"""
+    if not fp_to_check.exists():
+        raise FileNotFoundError(f"File '{fp_to_check}' does not exist!")
+
+
+def load_urls(urls_fp: Path) -> Sequence[Student]:
     """Load student Git URLs"""
 
     # The student list, initially empty
     students: list[Student] = []
 
     # Open Git URLs file
-    with open(urls_file) as ufile:
+    with open(urls_fp) as urls_file:
 
         # Cycle through each line of the file
-        for lno, line in enumerate(ufile, 1):
+        for lno, line in enumerate(urls_file, 1):
 
             # Remove leading and trailing whitespace
             line = line.strip()
@@ -180,7 +216,7 @@ def load_urls(urls_file: Path) -> Sequence[Student]:
             # Split line and check that it's composed of two chunks
             std_url = line.split()
             if len(std_url) != 2:
-                raise SyntaxError(f"Syntax error in line {lno} of {urls_file}: "\
+                raise SyntaxError(f"Syntax error in line {lno} of {urls_fp}: "\
                     f"'{line}'")
 
             # Create a student instance with it's ID and URL, taken from the line
