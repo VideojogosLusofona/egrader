e9ef3a274b94a12d52a285ae7f804baa2f9f5b20
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-06-13 17:32:40 +0100

    New cli: add args debug flag for outputting full exception traceback

--- egrader/cli.py
+++ egrader/cli.py
@@ -29,6 +29,14 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
+    # Debug flag for showing full stack traces when errors occur
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="show complete exception traceback when an error occurs"
+    )
+
+    # Allow for subcommands
     subparsers = parser.add_subparsers(
         title="commands",
         required=True
@@ -37,31 +45,31 @@ def main():
     # Create the parser for the "fetch" command
     parser_fetch = subparsers.add_parser(
         "fetch",
-        help="Fetch all repositories"
+        help="fetch all repositories"
     )
     parser_fetch.add_argument(
         f"-{OPT_E_SHORT}", f"--{OPT_E_LONG}",
         choices=[OPT_E_STOP, OPT_E_UPDT, OPT_E_OVWR],
-        help=f"Action to take when fetch has already been performed before (default: {OPT_E_STOP})",
+        help=f"action to take when fetch has already been performed before (default: {OPT_E_STOP})",
         default=OPT_E_STOP
     )
 
     parser_fetch.add_argument(
         "urls_file",
         metavar="URLS",
-        help="Student public Git account URLs file in TSV format",
+        help="student public Git account URLs file in TSV format",
         nargs=1
     )
     parser_fetch.add_argument(
         "rules_file",
         metavar="RULES",
-        help="Assessment rules in YAML format",
+        help="assessment rules in YAML format",
         nargs=1
     )
     parser_fetch.add_argument(
         "output_folder",
         metavar="OUTPUT",
-        help="Output folder (defaults to RULES minus yaml extension)",
+        help="output folder (defaults to RULES minus yaml extension)",
         nargs='?'
     )
     parser_fetch.set_defaults(func=fetch)
@@ -69,18 +77,18 @@ def main():
     # Create the parser for the "assess" command
     parser_assess = subparsers.add_parser(
         "assess",
-        help="Perform assessment"
+        help="perform assessment"
     )
     parser_assess.add_argument(
         "rules_file",
         type=str,
         metavar="<RULES_FILE>",
-        help="Assessment rules in YAML format"
+        help="assessment rules in YAML format"
     )
     parser_assess.add_argument(
         "output_folder",
         metavar="OUTPUT",
-        help="Output folder (defaults to RULES minus yaml extension)",
+        help="output folder (defaults to RULES minus yaml extension)",
         nargs='?'
     )
     parser_assess.set_defaults(func=assess)
@@ -93,6 +101,9 @@ def main():
         args.func(args)
     except (FileNotFoundError, FileExistsError, SyntaxError) as e:
         print(e.args[0], file=sys.stderr)
+        if (args.debug):
+            print("-------- Exception details --------", file=sys.stderr)
+            raise e
         return 1
     else:
         print("Program terminated successfully.")
@@ -176,7 +187,7 @@ def load_yaml(yaml_fp: Path) -> Any:
         with open(yaml_fp, "r") as yaml_file:
             yaml_obj = yaml.safe_load(yaml_file)
     except yaml.scanner.ScannerError as se:
-        raise SyntaxError(f"Syntax error in file {yaml_fp} {se.args[0]}") from se
+        raise SyntaxError(f"Syntax error{se.problem_mark} {se.context}: {se.problem}") from se
 
     return yaml_obj
 
