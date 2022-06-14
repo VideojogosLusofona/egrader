55c683b538fc2421c6759a07b4b32fc3b4d395d4
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-06-14 12:19:34 +0100

    Remove unused classes

--- egrader/Commit.py
+++
@@ -1,9 +0,0 @@
-class Commit:
-    """A commit."""
-
-    def __init__(self, sha, message, name, email, datetime):
-        self.sha = sha
-        self.message = message
-        self.name = name
-        self.email = email
-        self.datetime = datetime
