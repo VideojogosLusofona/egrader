737cf5236b24e64f8f7ac824ab2e3625ec5a1e61
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-06-15 17:58:37 +0100

    assess: first steps, read student valid URLs file

--- egrader/assess.py
+++ egrader/assess.py
@@ -1,3 +1,32 @@
+from pathlib import Path
+
+from .common import check_required_fp_exists, get_output_fp, get_valid_student_urls_fp
+from .yaml import load_yaml
+
+
 def assess(args) -> None:
-    print("ASSESS!")
-    print(args)
+    """Perform student assessment"""
+
+    # Determine rules file path
+    rules_fp: Path = Path(args.rules_file[0])
+
+    # Check if rules file exists, and if not, quit
+    check_required_fp_exists(rules_fp)
+
+    # Determine output folder, either given by user or we extract it from the
+    # rules file name
+    output_fp: Path = get_output_fp(args.output_folder, rules_fp)
+
+    # Check if output folder exists, and if not, quit
+    check_required_fp_exists(output_fp)
+
+    # Determine file path for valid student URLs yaml file
+    student_urls_fp: Path = get_valid_student_urls_fp(output_fp)
+
+    # Check if valid student URLs yaml file exists, and if not, quit
+    check_required_fp_exists(student_urls_fp)
+
+    # Load student list and their URLs
+    students = load_yaml(student_urls_fp, safe=False)
+
+    print(students)
