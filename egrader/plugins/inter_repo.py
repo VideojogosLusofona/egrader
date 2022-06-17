4e32633f5a00f02de4e726c368ee7fac18778fa6
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-06-17 22:13:18 +0100

    Implement example inter-assessment providing bonuses by no. commits

--- egrader/plugins/inter_repo.py
+++ egrader/plugins/inter_repo.py
@@ -1,6 +1,6 @@
-from typing import Sequence
+from typing import List, Sequence, Tuple
 
-# from ..git import git_at
+from ..git import git_at
 
 
 def assess_more_commits_bonus(
@@ -8,10 +8,34 @@ def assess_more_commits_bonus(
 ) -> Sequence[float]:
     """Add bonuses to repositories with more commits."""
 
-    return len(repo_paths) * [ 0.123 ]
+    # Number of repositories to inter-assess
+    n_repos = len(repo_paths)
 
-    # n_commits = git_at(repo_path, "rev-list", "HEAD", "--count")
-    # if int(n_commits) >= minimum:
-    #     return 1
-    # else:
-    #     return 0
+    # Make bonus list the same size as the number of repositories
+    bonus_lst: Sequence[float]
+
+    if len(bonuses) > n_repos:
+        bonus_lst = bonuses[:n_repos]
+    elif len(bonuses) < n_repos:
+        bonus_lst = list(bonuses) + (n_repos - len(bonuses)) * [0.0]
+    else:
+        bonus_lst = bonuses[:]
+
+    # Determine number of commits in each repository and associate them with
+    # the repositories index in repo_paths
+    idx_commits: List[Tuple[int, int]] = list(
+        enumerate([int(git_at(rp, "rev-list", "HEAD", "--count")) for rp in repo_paths])
+    )
+
+    # Sort by number of commits, higher to lower
+    idx_commits.sort(key=lambda ic: -ic[1])
+
+    # Associate the repo indexes (sorted by number of commits) with the
+    # user-specified bonuses
+    idx_bonus = list(zip([ic[0] for ic in idx_commits], bonus_lst))
+
+    # Resort bonuses by repo index
+    idx_bonus.sort()
+
+    # Return bonuses associated with each repo
+    return [ib[1] for ib in idx_bonus]
