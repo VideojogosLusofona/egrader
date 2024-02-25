e2f16375bf8f942c8ed36a88d4cef19652942969
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-02-25 17:12:06 +0000

    Config default user email and name for Git-related tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,6 @@ from egrader.git import git_at
 @pytest.fixture
 def git_repo_empty(tmp_path):
     git_at(tmp_path, "init")
+    git_at(tmp_path, "config", "user.email", "github-actions[bot]@users.noreply.github.com")
+    git_at(tmp_path, "config", "user.name", "github-actions")
     return tmp_path
