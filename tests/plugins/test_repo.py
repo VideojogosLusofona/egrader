d065f43539c2c0435aa7be6729583bfdc4136bc3
Nuno Fachada <faken@fakenmc.com>
2024-03-03 19:32:06 +0000

    Add more tests to assess_commit_date_interval, including strict

--- tests/plugins/test_repo.py
+++ tests/plugins/test_repo.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 
 import numpy as np
+import pytest
 
 from egrader.plugins.repo import assess_commit_date_interval, assess_min_commits
 from egrader.types import StudentGit
@@ -26,7 +27,8 @@ def test_repo_assess_min_commits_yes(git_repo, make_commit):
     assert result == 1
 
 
-def test_repo_assess_commit_date_interval(git_repo, make_commit):
+@pytest.mark.parametrize("strict", [False, True])
+def test_repo_assess_commit_date_interval_some(git_repo, make_commit, strict):
     """Test if the percentage of commits done in a date interval is correct."""
     # Interval for valid commits
     before_date: datetime = datetime.now() - timedelta(days=1)
@@ -52,9 +54,77 @@ def test_repo_assess_commit_date_interval(git_repo, make_commit):
     # Check if plugin returns the expected result
     n_commits = n_commits_before + n_commits_within + n_commits_after
     stdgit = StudentGit("", "", "")
-    np.testing.assert_allclose(
-        n_commits_within / n_commits,
-        assess_commit_date_interval(
-            stdgit, str(git_repo), before_date=before_date, after_date=after_date
-        ),
+
+    grade = assess_commit_date_interval(
+        stdgit,
+        str(git_repo),
+        before_date=before_date,
+        after_date=after_date,
+        strict=strict,
     )
+
+    if strict:
+        assert grade == 0
+    else:
+        np.testing.assert_allclose(
+            grade,
+            n_commits_within / n_commits,
+        )
+
+
+@pytest.mark.parametrize("strict", [False, True])
+def test_repo_assess_commit_date_interval_all(git_repo, make_commit, strict):
+    """Test that all commits are done in the specified date interval."""
+    # Interval for valid commits
+    before_date: datetime = datetime.now() - timedelta(days=1)
+    after_date: datetime = datetime.now() - timedelta(days=4)
+
+    # Commits done within the valid time interval
+    make_commit(git_repo, after_date + timedelta(seconds=30))
+    make_commit(git_repo, after_date + timedelta(hours=1, minutes=30))
+    make_commit(git_repo, after_date + timedelta(hours=1, minutes=45))
+    make_commit(git_repo, before_date - timedelta(days=1, hours=5))
+    make_commit(git_repo, before_date - timedelta(minutes=45))
+    make_commit(git_repo, before_date - timedelta(seconds=30))
+
+    # Check if plugin returns the expected result
+    stdgit = StudentGit("", "", "")
+
+    grade = assess_commit_date_interval(
+        stdgit,
+        str(git_repo),
+        before_date=before_date,
+        after_date=after_date,
+        strict=strict,
+    )
+
+    assert grade == 1
+
+
+@pytest.mark.parametrize("strict", [False, True])
+def test_repo_assess_commit_date_interval_none(git_repo, make_commit, strict):
+    """Test that no commits are done in the specified date interval."""
+    # Interval for valid commits
+    before_date: datetime = datetime.now() - timedelta(days=1)
+    after_date: datetime = datetime.now() - timedelta(days=4)
+
+    # Commits done outside the valid time interval
+    make_commit(git_repo, after_date - timedelta(hours=1, minutes=45))
+    make_commit(git_repo, after_date - timedelta(hours=1, minutes=30))
+    make_commit(git_repo, after_date - timedelta(seconds=30))
+    make_commit(git_repo, before_date + timedelta(seconds=30))
+    make_commit(git_repo, before_date + timedelta(minutes=45))
+    make_commit(git_repo, before_date + timedelta(days=1, hours=5))
+
+    # Check if plugin returns the expected result
+    stdgit = StudentGit("", "", "")
+
+    grade = assess_commit_date_interval(
+        stdgit,
+        str(git_repo),
+        before_date=before_date,
+        after_date=after_date,
+        strict=strict,
+    )
+
+    assert grade == 0
