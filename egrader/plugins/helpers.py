27c141aa7611758c08bed5d3b4ae6e30b636dcc8
Nuno Fachada <faken@fakenmc.com>
2024-03-03 19:27:37 +0000

    Fix bug when interpreting datetimes

--- egrader/plugins/helpers.py
+++ egrader/plugins/helpers.py
@@ -10,12 +10,12 @@ def interpret_datetime(obj: date | datetime | str, tzi: tzinfo | None) -> dateti
     """Try to convert an object into a [datetime][datetime.datetime] instance."""
     dt: datetime
 
-    if isinstance(obj, date):
-        dt = datetime.combine(cast(date, obj), datetime.min.time())
+    if isinstance(obj, str):
+        dt = parser.parse(cast(str, obj))
     elif isinstance(obj, datetime):
         dt = cast(datetime, obj)
-    elif isinstance(obj, str):
-        dt = parser.parse(cast(str, obj))
+    elif isinstance(obj, date):
+        dt = datetime.combine(cast(date, obj), datetime.min.time())
     else:
         raise SyntaxError(f"Unable to parse date/time given by {obj!r}")
 
