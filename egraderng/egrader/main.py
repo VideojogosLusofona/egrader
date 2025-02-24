136606a8ac35c716b415f2105c482df7c4e3a4ea
Nuno Fachada <faken@fakenmc.com>
2025-02-24 17:46:25 +0000

    NG: Add main options to the three commands

--- egraderng/egrader/main.py
+++ egraderng/egrader/main.py
@@ -1,34 +1,94 @@
 """The egrader command-line interface."""
 
+from enum import Enum
+from pathlib import Path
+
 import typer
+from typing_extensions import Annotated
 
 app = typer.Typer()
 
 
+class FetchMode(str, Enum):
+    """Assessment modes."""
+
+    stop = "stop"
+    update = "update"
+    overwrite = "overwrite"
+
+
 @app.callback()
 def callback():
-    """Callback for invoking egrader as a typer app."""
+    """Callback for invoking egrader as a Typer app."""
 
 
 @app.command()
-def fetch(name: str):
+def fetch(
+    urls: Annotated[
+        Path,
+        typer.Argument(
+            help="TSV file with remote Git account URLs.", show_default=False
+        ),
+    ],
+    rules: Annotated[
+        Path,
+        typer.Argument(help="YAML file with assessment rules.", show_default=False),
+    ],
+    assess_folder: Annotated[
+        Path | None,
+        typer.Argument(
+            help="Folder where to place assessment results.",
+            show_default="`rules` minus the .yaml extension.",
+        ),
+    ] = None,
+    mode: Annotated[
+        FetchMode,
+        typer.Option(
+            help="Action to take when one or more repositories to"
+            + "fetch already exist locally."
+        ),
+    ] = FetchMode.stop,
+    wait: Annotated[
+        float,
+        typer.Option(
+            "--wait", "-w", help="Time in seconds to wait between clones/pulls."
+        ),
+    ] = 0.0,
+):
     """Fetch projects from repositories."""
-    print(f"Hello {name}")
+    print(f"{urls}\n{rules}\n{assess_folder}")
 
 
 @app.command()
-def assess(name: str, formal: bool = False):
+def assess(
+    rules: Annotated[
+        Path,
+        typer.Argument(help="YAML file with assessment rules.", show_default=False),
+    ],
+    assess_folder: Annotated[
+        Path | None,
+        typer.Argument(
+            help="Folder containing assessment results.",
+            show_default="`rules` minus the .yaml extension.",
+        ),
+    ] = None,
+):
     """Assess fetched projects."""
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+    print(f"{rules}\n{assess_folder}")
 
 
 @app.command()
-def report(name: str):
+def report(
+    assess_folder: Annotated[
+        Path,
+        typer.Argument(
+            help="Folder containing assessment results.",
+            show_default=False,
+        ),
+    ],
+):
     """Generate an assessment report."""
-    print(f"Hello {name}")
+    print(f"Hello {assess_folder}")
 
 
 @app.command()
