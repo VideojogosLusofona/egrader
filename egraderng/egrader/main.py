072a6beaf757377bc579ec42ae090252b237f0e4
Nuno Fachada <nuno.fachada@ulusofona.pt>
2025-02-25 19:50:20 +0000

    NG: Add version flag

--- egraderng/egrader/main.py
+++ egraderng/egrader/main.py
@@ -1,8 +1,30 @@
 """The egrader command-line interface."""
 
+import importlib.metadata
+
+import typer
+
 from .cli import app
+
+__version__ = importlib.metadata.version("egrader")
+
+
+def version_callback(value: bool):
+    """Show version."""
+    if value:
+        print(f"egrader v{__version__}")
+        raise typer.Exit()
 
 
 @app.callback()
-def callback():
+def callback(
+    version: bool | None = typer.Option( # noqa
+        None,
+        "--version",
+        "-v",
+        help="Show the application version",
+        callback=version_callback,
+        is_eager=True,
+    )
+):
     """Callback for invoking egrader as a Typer app."""
