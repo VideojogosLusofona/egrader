91f6095dc0da04227c6f0ac8677fe67fddc3518b
Nuno Fachada <nuno.fachada@ulusofona.pt>
2025-02-27 17:16:00 +0000

    NG: Add copyright header to all files

--- egraderng/egrader/cli/__init__.py
+++ egraderng/egrader/cli/__init__.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2022-2025 Nuno Fachada and contributors
+# Licensed under the GNU General Public License v3.0 or later.
+# See <https://www.gnu.org/licenses/gpl-3.0.html> for details.
+
 """CLI commands."""
 
 import typer
--- egraderng/egrader/core/__init__.py
+++ egraderng/egrader/core/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (C) 2022-2025 Nuno Fachada and contributors
+# Licensed under the GNU General Public License v3.0 or later.
+# See <https://www.gnu.org/licenses/gpl-3.0.html> for details.
+
 """Core functionality."""
