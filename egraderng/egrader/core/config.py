cc0d69ff54c32d6c3e4a495ec1c54fadef0e8c52
Nuno Fachada <nuno.fachada@ulusofona.pt>
2025-02-27 15:06:34 +0000

    NG: Implement mutable config instead of constants

---
+++ egraderng/egrader/core/config.py
@@ -0,0 +1,8 @@
+"""App-wide configuration."""
+
+from types import SimpleNamespace
+
+# Create a global config object
+eg_config = SimpleNamespace()
+eg_config.id_col = "id"
+eg_config.repo_col = "repo"
