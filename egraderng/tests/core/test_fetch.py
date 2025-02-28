7f1eda06bc49428c2201283f95ebd22c9c426a78
Nuno Fachada <nuno.fachada@ulusofona.pt>
2025-02-28 00:07:46 +0000

    NG: add faker, data load tests (wip)

--- egraderng/tests/core/test_fetch.py
+++ egraderng/tests/core/test_fetch.py
@@ -2,4 +2,13 @@
 # Licensed under the GNU General Public License v3.0 or later.
 # See <https://www.gnu.org/licenses/gpl-3.0.html> for details.
 
-"""Tests for the egrader application."""
+"""Test the core fetch operations."""
+
+
+import pytest
+
+from egrader.core import _load_repos
+
+
+def test_load_repos(ver_opt):
+    """Test the function to load the repos file."""
